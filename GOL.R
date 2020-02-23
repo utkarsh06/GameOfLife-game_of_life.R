602b4d32e83072ad58e05f93292da5c489025f7b
Utkarsh Gaur <31377783+utkarsh06@users.noreply.github.com>
2020-02-23 01:12:52 -0500

    Update GOL.R

--- GOL.R
+++ GOL.R
@@ -1,48 +1,46 @@
 #Gamne of Life
 
 apply_rules <- function(matr){
-  counter <- 0
-  for(l in 1:nrow(matr)){
-    for(k in 1:ncol(matr)){
-      if(matr[l,k] == 1 & (l != 2 & k!=2)){ #Also make sure i != j
-        counter <- counter +1 
-      }
-  }
-  }
-  
-  if(counter < 2 | counter >3){
-    return(0)
-  } 
-  else{
-    return(1)
-  }
-  
+ 
+ population <- matr[1,1] + matr[1,2]+ matr[1,3]+ matr[2,1]+ matr[2,3]+ matr[3,1]+ matr[3,2]+ matr[3,3]
+ #waprint(population)
+ if(population == 3 | population == 2){
+   return(1)
+ }
+ else{ return(0)
+ }
+ 
 }
 
 
 
-next_gen <- function(oldstate){
-  new_state <- matrix( , nrow =nrow(oldstate), ncol = ncol(oldstate))
-  for( i in 1:(nrow(oldstate))){
-    for(j in 1:(ncol(oldstate))){
-      if( (i !=1 & j != 1) &(i !=1 & j != 0) &(i !=0 & j != 1)& (i !=0 & j != 0)){
-      new_state[i,j] <- apply_rules(oldstate[i-1:i+1,j-1:j+1])
-      }
+next_gen <- function(oldState){
+  new_state <- matrix( runif(nrow(oldState)*ncol(oldState), min =0,max =0) , nrow = nrow(oldState), ncol = ncol(oldState))
+  for( i in 2: (nrow(oldState)-1)){
+    for(j in 2: (ncol(oldState)-1)){
+     
+      new_state[i,j] <- apply_rules(oldState[c(i-1,i,i+1) , c(j-1,j,j+1)])
+      
     }
   }
+  
   return(new_state)
 }
 
 game_of_life <- function(N,M,p,nGen){
-  initialState <- matrix(runif(M*N), ncol = N)
+  initialState <- matrix(runif(N*M), nrow = N)
   initialState[initialState < p] <- 0
   initialState[initialState > p] <- 1
+  image(initialState)
   for(i in 1:nGen){
-    initialState <- (next_gen(oldstate = initialState))
-    image(((next_gen(initialState))))
+    initialState <- next_gen(initialState)
+    image((initialState))
+    
     }
-}
+} 
 
-game_of_life(20,20,0.4,10)
 
+
+
+game_of_life(250,250,.95,40)
 
